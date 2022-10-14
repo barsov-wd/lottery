8bbaec68fbaac0605ac2c38c1800289077795947
Victor <barosv@yandex.ru>
2022-10-14 16:06:36 +0400

    Сверстан проект без адаптива.

---
+++ src/js/script.js
@@ -0,0 +1,21 @@
+new Swiper('.swiper', {
+    navigation: {
+        nextEl: '.slider__arrows--next',
+        prevEl: '.slider__arrows--prev'
+    },
+
+    slidesPerView: 3,
+
+    breakpoints: {
+        320: {
+            slidesPerView: 1,
+            spaceBetween: 10,
+        },
+        768: {
+            slidesPerView: 2
+        },
+        992: {
+            slidesPerView: 4
+        }
+    }
+});
